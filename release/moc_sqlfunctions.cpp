7a0339d309fb13bebf98e496b5310a161fa46855
root <root@tangboshi-Aspire-V5-572G>
2015-05-12 01:07:36 +0200

    Added everything to the repo once again

---
+++ release/moc_sqlfunctions.cpp
@@ -0,0 +1,232 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'sqlfunctions.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../KinectShopApp/sqlfunctions.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'sqlfunctions.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_sqlfunctions_t {
+    QByteArrayData data[30];
+    char stringdata[310];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_sqlfunctions_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_sqlfunctions_t qt_meta_stringdata_sqlfunctions = {
+    {
+QT_MOC_LITERAL(0, 0, 12),
+QT_MOC_LITERAL(1, 13, 12),
+QT_MOC_LITERAL(2, 26, 0),
+QT_MOC_LITERAL(3, 27, 15),
+QT_MOC_LITERAL(4, 43, 4),
+QT_MOC_LITERAL(5, 48, 13),
+QT_MOC_LITERAL(6, 62, 15),
+QT_MOC_LITERAL(7, 78, 7),
+QT_MOC_LITERAL(8, 86, 9),
+QT_MOC_LITERAL(9, 96, 15),
+QT_MOC_LITERAL(10, 112, 9),
+QT_MOC_LITERAL(11, 122, 8),
+QT_MOC_LITERAL(12, 131, 9),
+QT_MOC_LITERAL(13, 141, 12),
+QT_MOC_LITERAL(14, 154, 6),
+QT_MOC_LITERAL(15, 161, 4),
+QT_MOC_LITERAL(16, 166, 4),
+QT_MOC_LITERAL(17, 171, 10),
+QT_MOC_LITERAL(18, 182, 12),
+QT_MOC_LITERAL(19, 195, 8),
+QT_MOC_LITERAL(20, 204, 12),
+QT_MOC_LITERAL(21, 217, 8),
+QT_MOC_LITERAL(22, 226, 8),
+QT_MOC_LITERAL(23, 235, 5),
+QT_MOC_LITERAL(24, 241, 11),
+QT_MOC_LITERAL(25, 253, 14),
+QT_MOC_LITERAL(26, 268, 12),
+QT_MOC_LITERAL(27, 281, 13),
+QT_MOC_LITERAL(28, 295, 6),
+QT_MOC_LITERAL(29, 302, 6)
+    },
+    "sqlfunctions\0purchaseDone\0\0vector<product>\0"
+    "cart\0adminLoggedIn\0isAlreadyInCart\0"
+    "product\0myProduct\0listAllProducts\0"
+    "addToCart\0showCart\0clearCart\0changeAmount\0"
+    "string\0mode\0diff\0checkStock\0checkBalance\0"
+    "purchase\0registerUser\0username\0password\0"
+    "login\0empowerUser\0disempowerUser\0"
+    "listAllUsers\0refillBalance\0amount\0"
+    "testJs\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_sqlfunctions[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      19,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  109,    2, 0x06,
+       5,    0,  112,    2, 0x06,
+
+ // slots: name, argc, parameters, tag, flags
+       6,    1,  113,    2, 0x0a,
+       9,    0,  116,    2, 0x0a,
+      10,    1,  117,    2, 0x0a,
+      11,    0,  120,    2, 0x0a,
+      12,    0,  121,    2, 0x0a,
+      13,    2,  122,    2, 0x0a,
+      13,    3,  127,    2, 0x0a,
+      17,    0,  134,    2, 0x0a,
+      18,    0,  135,    2, 0x0a,
+      19,    0,  136,    2, 0x0a,
+      20,    2,  137,    2, 0x0a,
+      23,    2,  142,    2, 0x0a,
+      24,    0,  147,    2, 0x0a,
+      25,    0,  148,    2, 0x0a,
+      26,    0,  149,    2, 0x0a,
+      27,    1,  150,    2, 0x0a,
+      29,    0,  153,    2, 0x0a,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
+
+ // slots: parameters
+    0x80000000 | 7, 0x80000000 | 7,    8,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 7, 0x80000000 | 14,    8,   15,
+    QMetaType::Void, 0x80000000 | 7, QMetaType::Int, 0x80000000 | 14,    8,   16,   15,
+    QMetaType::Int,
+    QMetaType::Double,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,   21,   22,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,   21,   22,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   28,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void sqlfunctions::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        sqlfunctions *_t = static_cast<sqlfunctions *>(_o);
+        switch (_id) {
+        case 0: _t->purchaseDone((*reinterpret_cast< vector<product>(*)>(_a[1]))); break;
+        case 1: _t->adminLoggedIn(); break;
+        case 2: { product _r = _t->isAlreadyInCart((*reinterpret_cast< product(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< product*>(_a[0]) = _r; }  break;
+        case 3: _t->listAllProducts(); break;
+        case 4: _t->addToCart((*reinterpret_cast< product(*)>(_a[1]))); break;
+        case 5: _t->showCart(); break;
+        case 6: _t->clearCart(); break;
+        case 7: _t->changeAmount((*reinterpret_cast< product(*)>(_a[1])),(*reinterpret_cast< string(*)>(_a[2]))); break;
+        case 8: _t->changeAmount((*reinterpret_cast< product(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< string(*)>(_a[3]))); break;
+        case 9: { int _r = _t->checkStock();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = _r; }  break;
+        case 10: { double _r = _t->checkBalance();
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = _r; }  break;
+        case 11: _t->purchase(); break;
+        case 12: _t->registerUser((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 13: _t->login((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 14: _t->empowerUser(); break;
+        case 15: _t->disempowerUser(); break;
+        case 16: _t->listAllUsers(); break;
+        case 17: _t->refillBalance((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: _t->testJs(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (sqlfunctions::*_t)(vector<product> );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&sqlfunctions::purchaseDone)) {
+                *result = 0;
+            }
+        }
+        {
+            typedef void (sqlfunctions::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&sqlfunctions::adminLoggedIn)) {
+                *result = 1;
+            }
+        }
+    }
+}
+
+const QMetaObject sqlfunctions::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_sqlfunctions.data,
+      qt_meta_data_sqlfunctions,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *sqlfunctions::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *sqlfunctions::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_sqlfunctions.stringdata))
+        return static_cast<void*>(const_cast< sqlfunctions*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int sqlfunctions::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 19)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 19;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 19)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 19;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void sqlfunctions::purchaseDone(vector<product> _t1)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void sqlfunctions::adminLoggedIn()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, 0);
+}
+QT_END_MOC_NAMESPACE
